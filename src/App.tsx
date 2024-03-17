e6faeac34441a6c4a8c28f50bc7dfe3801b92f5c
miawithcode <cuttontail@gmail.com>
2024-03-17 22:37:45 +0800

    feat: add main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
+import Container from "./components/Container";
 import Header from "./components/Header";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main>
+        {/* Hero */}
+        <div>
+          <Container>Hero Component</Container>
+        </div>
+
+        {/* usps */}
+        <div>
+          <Container> </Container>
+        </div>
+
+        {/* 3 cols layout */}
+        <div>
+          <Container> </Container>
+        </div>
+
+        {/* Carousel with posters */}
+        <div>
+          <Container> </Container>
+        </div>
+      </main>
     </>
   );
 };

19d35e835d1dc7e2f3fe2e04a8b4282ea4ad7d46
miawithcode <cuttontail@gmail.com>
2024-03-18 02:18:51 +0800

    feat: hero image scroll fade out

--- src/components/section/Hero.tsx
+++ src/components/section/Hero.tsx
@@ -1,15 +1,28 @@
+import { motion, useScroll, useTransform } from "framer-motion";
 import Container from "../../components/Container";
 import Button from "../Button";
+import { useRef } from "react";
 
 const Hero = () => {
+  const videoContainerRef = useRef<HTMLDivElement>(null);
+  const { scrollYProgress } = useScroll({
+    target: videoContainerRef,
+    offset: ["start start", "end end"],
+  });
+  const opacity = useTransform(scrollYProgress, [0, 0.3, 1], [1, 1, 0]);
+
   return (
-    <div className="h-[300vh] bg-background relative text-white">
-      <div className="absolute -top-[--header-total-height] left-0 h-[200vh] w-full">
+    <div className="bg-background relative h-[300vh] text-white">
+      <motion.div
+        style={{ opacity }}
+        ref={videoContainerRef}
+        className="absolute -top-[--header-total-height] left-0 h-[200vh] w-full"
+      >
         <img
-          src="/posters/boy-showering.jpg"
+          src="/posters/sea.jpg"
           className="sticky top-0 h-screen w-full object-cover"
         ></img>
-      </div>
+      </motion.div>
 
       <Container className="relative z-10 flex min-h-[--hero-height] flex-col items-start justify-end pb-7">
         <h1 className="mb-10 text-5xl font-bold">

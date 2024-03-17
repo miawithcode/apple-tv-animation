865880936fff49f64f8797aaececb3ac729a1173
miawithcode <cuttontail@gmail.com>
2024-03-17 22:10:10 +0800

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <>
+      <Header />
+    </>
+  );
+};
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <>
+      <header className="bg-backgroundContrast flex min-h-11 items-center px-6 text-white">
+        <a href="/" className="-ml-6 flex h-11 items-center px-6">
+          <span className="sr-only">Back to homepage</span>
+        </a>
+      </header>
+      <div className="bg-backgroundContrast sticky top-0 flex min-h-11 items-center px-6 text-white">
+        Apple TV+
+      </div>
+    </>
+  );
+};
+export default Header;

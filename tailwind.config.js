d371dd886516e4de041b7aa0f3a6112715db9116
miawithcode <cuttontail@gmail.com>
2024-03-17 21:45:40 +0800

    style: add design color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      background: "#000",
+      backgroundContrast: "#111",
+      black: "#1d1d1f",
+      white: "#fff",
+      gray: "#f5f5f7",
+      link: "2997ff",
+    },
     extend: {},
   },
   plugins: [],
